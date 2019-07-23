bee329548cd5d67ce6ec7f12aa9f5b25b18c2f8e
shubratha <shubratha.leo@gmail.com>
2019-07-23 19:02:14 +0530

    layout.js corrected

--- mysite/frontend/src/containers/Layout.js
+++ mysite/frontend/src/containers/Layout.js
@@ -3,7 +3,7 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
 
 const { Header, Content, Footer } = Layout;
-class CustomLayout = (props) => {
+const CustomLayout = (props) => {
   return (
       <Layout className="layout">
         <Header>
